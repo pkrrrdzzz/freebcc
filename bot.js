90bb4bd2b4cc6f7c0766ad05ac3de08ff87f2a0c
pkrrrdzzz <48836718+pkrrrdzzz@users.noreply.github.com>
2019-03-24 13:19:51 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,40 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '.'
-
-client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
-client.user.setGame(poker dz,"http://twitch.tv/S-F")
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log([Start] ${new Date()});
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(Logged in as * [ " ${client.user.username} " ]);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(servers! [ " ${client.guilds.size} " ]);
-  console.log(Users! [ " ${client.users.size} " ]);
-  console.log(channels! [ " ${client.channels.size} " ]);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = [' $help','Poker dz';
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/iXMiDo`);
+    }, ms);
+  console.log('---------------');
+  console.log('poker -Bot Is Online')
+  console.log('---------------')
 });
-
-
-client.on('ready', () => {
-
-    client.user.setActivity("By Poker | $help",{type: 'Streaming'})
-
-});
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
