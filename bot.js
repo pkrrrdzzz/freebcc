c5d9adf75aa197844e59e6b8a28f37eb8168e2cc
pkrrrdzzz <48836718+pkrrrdzzz@users.noreply.github.com>
2019-03-24 13:33:08 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,6 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const client = new Discord.Client();
-client.on('ready', function(){
-    var ms = 10000 ;
-    var setGame = [' $help','Poker dz';
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/iXMiDo`);
-    }, ms);
-  console.log('---------------');
-  console.log('poker -Bot Is Online')
-  console.log('---------------')
-});
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
