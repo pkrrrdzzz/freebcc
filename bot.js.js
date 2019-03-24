75d4e1227196a9155e69018d7872790f3d6f763a
pkrrrdzzz <48836718+pkrrrdzzz@users.noreply.github.com>
2019-03-24 12:25:38 +0100

    Update bot.js.js

--- bot.js.js
+++ bot.js.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 const prefix = '.'
 
 client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
+
 client.user.setGame(By Poker,"http://twitch.tv/S-F")
   console.log('')
   console.log('')
